9b3886cb9482571842fc15e8b5d51ef420765aca
Larissa Lopes <larissasantos.lopes@hotmail.com>
2023-01-23 22:13:58 -0300

    finalizado criação do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,25 @@
+import React from "react";
+import Logo from "../../assets/image/logo.png"
 import './style.css';
+// import { Link } from "react-router-dom";
 
 function Header(){
     return(
-        <header>
-            
-
+        <header id="menu">
+            <div className="wrapper">
+                <img className="logo" src={Logo} alt="Logo Spotify" />
+                
+                <nav>
+                    <a href="">Premium</a>
+                    <a href="">Suporte</a>
+                    <a href="">Baixar  </a>
+                    <span className="divisor"></span>
+                    <a href="">  Inscrever-se</a>
+                    <a href="">Entrar</a> 
+                </nav>
+            </div>
         </header>
     )
-}
+};
 
-export default Header
+export default Header;
